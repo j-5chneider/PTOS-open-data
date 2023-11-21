84321652d6712220db7558ae00e37292ac96d7f9
j-5chneider <j.schn@posteo.de>
2023-11-21 21:25:55 +0100

    first commit full book

---
+++ www/download_references_bib.R
@@ -0,0 +1,3 @@
+### DOWNLOAD NEWEST SOURCES
+download.file("https://drive.google.com/uc?export=download&id=1ASV99YHMHSNChSNzqbZwvz2SiqYvmqO_", # befindet sich im DIPF Ordner
+              "references.bib", overwrite = T)
